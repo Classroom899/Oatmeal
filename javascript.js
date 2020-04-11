e01d260c6e835ff17ac968f2e6724657aa252af7
Computer Desktop <60677031+Classroom899@users.noreply.github.com>
2020-04-10 19:08:15 -0700

    Add Javascript files practice

---
+++ javascript.js
@@ -0,0 +1,29 @@
+var name ;  //Quotes = Strings//
+var isSleeping = true;
+var catCount = 1;
+var user;
+
+var hasCat = confirm("Do you have a cat?")
+if (hasCat === true){
+    name = prompt("What is your cat's name?")
+}
+
+
+
+//var animalPreference = prompt("Do you prefer cats or dogs")
+
+if (name === "Oatmeal"){
+    alert("That's my cat")
+}
+else if (name === "Max"){
+    alert("That's Max")
+}
+else { 
+    alert("That's not my cat")
+}
+
+
+console.log(isSleeping)
+console.log()
+
+
